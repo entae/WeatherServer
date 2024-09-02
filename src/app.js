3e4af862308d9f7dd68553b5ee211c592ba425dc
In Tae Chung <intae29@gmail.com>
2024-09-02 01:11:12 -0400

    replaced static pages with handlebar views to make dynamic

--- src/app.js
+++ src/app.js
@@ -4,9 +4,33 @@ const express = require('express')
 // used one time to create an app
 // then we can use functions from this app to help create our page
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
+
+// rendering dynamic elements using handlebar
+// also lets us use a template engine
+app.set('view engine', 'hbs')
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'InTae Chung'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'help',
+        message: 'Do you require assistance?'
+    })
+})
 
 // /weather
 app.get('/weather', (req, res) => {
